d119383e626b0366cc1f2e0cb007ca6ac2081898
issacarbrun <issacarbrunow@gmail.com>
2022-05-05 11:08:49 -0300

    Authenticação

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken')
+
+const JWTSecret ="ashahsabsuh"
+
+app.use(cors());
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -31,10 +37,24 @@ var db = {
             year: 2004,
             price: 2014
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Issacar Brunow",
+            email: "issacarbrunow@gmail.com",
+            password: "abcq!2799"
+        },
+        {
+            id: 1,
+            name: "Augusto",
+            email: "augustobrunow@gmail.com",
+            password: "abcq!2799"
+        },
     ]
 }
 
-app.get("/games", (req, res ) => {
+app.get("/games",auth, (req, res ) => {
     res.statusCode = 200;
     res.json(db.games);
 })
@@ -113,6 +133,56 @@ app.put("/game/:id",(req,res) => {
     
 });
 
+app.post("/auth",(req,res) => {
+    var {email, password} = req.body;    
+
+    if(email != undefined) {
+        var user = db.users.find(u => u.email == email);
+        
+        if(user.password == password){
+            jwt.sign({id: user.id, email: user.id}, JWTSecret,{expiresIn: '48h'},(err,token) => {
+                if(err){
+                    res.status(400);
+                    res.json({err:"Falha interna"});
+                } else {
+                    res.status( 200 );
+                    res.json({token: token});
+                }
+            });
+        } else {
+            res.status(401);
+            res.json({err: "Credenciais inválidas"})
+        }
+
+    } else {
+        res.status(404);
+        res.json({err: "O email enviado é inválido"})
+    }
+});
+
+function auth(req,res,next){
+
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+        
+        jwt.verify(token,JWTSecret,(err,data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token Inválido"})
+            } else {
+                console.log(data)
+            }
+        })
+        
+    } else {
+        res.status(401);
+        res.json({err: "Token inválido"});
+    }
+    next();
+}
 
 app.listen(8000,() =>{
     console.log('API ATIVO');
